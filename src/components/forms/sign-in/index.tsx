62193270dc7f65e819c4ab9c9bfe5a8f3daf49eb
Durga Potnuru <vijaypotnuru123@gmail.com>
2025-04-14 23:23:34 +0530

    Remove package-lock.json and update React dependencies in package.json
    - Deleted package-lock.json to reset dependency tree.
    - Updated React and ReactDOM versions to 19.0.0-rc for compatibility with upcoming features.
    - Cleaned up formatting in SignInForm and SignUpForm components for improved readability.

--- src/components/forms/sign-in/index.tsx
+++ src/components/forms/sign-in/index.tsx
@@ -7,24 +7,23 @@ import { GROUPLE_CONSTANTS } from "@/constants";
 import { useAuthSignIn } from "@/hooks/authentication";
 
 const SignInForm = () => {
-  const { isPending, onAuthenticateUser, register, errors } = useAuthSignIn();
+	const { isPending, onAuthenticateUser, register, errors } = useAuthSignIn();
 
-  return (
-    <form className="mt-10 flex flex-col gap-3" onSubmit={onAuthenticateUser}>
-      {GROUPLE_CONSTANTS.signInForm.map((field) => (
-        <FormGenerator
-          {...field}
-          key={field.id}
-          register={register}
-          errors={errors}
-        />
-      ))}
-      <div id="clerk-captcha" />
-      <Button type="submit" className="rounded-2xl">
-        <Loader loading={isPending}>Sign In with Email</Loader>
-      </Button>
-    </form>
-  );
+	return (
+		<form className="mt-10 flex flex-col gap-3" onSubmit={onAuthenticateUser}>
+			{GROUPLE_CONSTANTS.signInForm.map((field) => (
+				<FormGenerator
+					{...field}
+					key={field.id}
+					register={register}
+					errors={errors}
+				/>
+			))}
+			<Button type="submit" className="rounded-2xl">
+				<Loader loading={isPending}>Sign In with Email</Loader>
+			</Button>
+		</form>
+	);
 };
 
 export default SignInForm;
--- src/components/forms/sign-up/index.tsx
+++ src/components/forms/sign-up/index.tsx
@@ -9,66 +9,63 @@ import dynamic from "next/dynamic";
 type Props = {};
 
 const OtpInput = dynamic(
-  () =>
-    import("@/components/global/otp-input").then(
-      (component) => component.default
-    ),
-  { ssr: false }
+	() =>
+		import("@/components/global/otp-input").then(
+			(component) => component.default,
+		),
+	{ ssr: false },
 );
 
 const SignUpForm = () => {
-  const {
-    register,
-    errors,
-    verifying,
-    creating,
-    onGenerateCode,
-    onInitiateUserRegistration,
-    code,
-    setCode,
-    getValues,
-  } = useAuthSignUp();
+	const {
+		register,
+		errors,
+		verifying,
+		creating,
+		onGenerateCode,
+		onInitiateUserRegistration,
+		code,
+		setCode,
+		getValues,
+	} = useAuthSignUp();
 
-  return (
-    <form
-      onSubmit={onInitiateUserRegistration}
-      className="mt-10 flex flex-col gap-3"
-    >
-      {verifying ? (
-        <div className="mb-5 flex justify-center">
-          <OtpInput otp={code} setOtp={setCode} />
-        </div>
-      ) : (
-        <>
-          {GROUPLE_CONSTANTS.signUpForm.map((field) => (
-            <FormGenerator
-              {...field}
-              key={field.id}
-              register={register}
-              errors={errors}
-            />
-          ))}
-          <div id="clerk-captcha" />
-        </>
-      )}
+	return (
+		<form
+			onSubmit={onInitiateUserRegistration}
+			className="mt-10 flex flex-col gap-3"
+		>
+			{verifying ? (
+				<div className="mb-5 flex justify-center">
+					<OtpInput otp={code} setOtp={setCode} />
+				</div>
+			) : (
+				GROUPLE_CONSTANTS.signUpForm.map((field) => (
+					<FormGenerator
+						{...field}
+						key={field.id}
+						register={register}
+						errors={errors}
+					/>
+				))
+			)}
 
-      {verifying ? (
-        <Button type="submit" className="rounded-2xl">
-          <Loader loading={creating}>Sign Up with Email</Loader>
-        </Button>
-      ) : (
-        <Button
-          type="button"
-          className="rounded-2xl"
-          onClick={() =>
-            onGenerateCode(getValues("email"), getValues("password"))
-          }
-        >
-          <Loader loading={false}>Generate Code</Loader>
-        </Button>
-      )}
-    </form>
-  );
+			{verifying ? (
+				<Button type="submit" className="rounded-2xl">
+					<Loader loading={creating}>Sign Up with Email</Loader>
+				</Button>
+			) : (
+				<Button
+					type="button"
+					className="rounded-2xl"
+					onClick={() =>
+						onGenerateCode(getValues("email"), getValues("password"))
+					}
+				>
+					<Loader loading={false}>Generate Code</Loader>
+				</Button>
+			)}
+		</form>
+	);
 };
 
 export default SignUpForm;
